5f658fb5c95333b75952ed043b04f8d2fb3ae4cb
Michael Kelly <cybernationnews@gmail.com>
2022-03-17 13:31:36 -0500

    Add comments throughout script, arranged code layout, and implemented an offset option for codon reading frame.

--- main.py
+++ main.py
@@ -3,74 +3,98 @@ from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
 
 # Function used to identify and keep track of incoming codons
-def TrackCodons(inputseq, codonmap):
-    length = len(inputseq)
-    start = 0
-    end = 2
-    codonsread = 0
+# Input: sequence, codon frequency dictionary, starting index
+def track_codons(inputseq, codonmap, offset):
+    length = len(inputseq)  # Saving length of current sequence
+    start = offset  # Starting index for codon frame read
+    end = start + 2  # Ending index for codon frame read
+    codonsread = 0  # Keeps track of codons read
 
+    # While the frame has three nucleotides to read
     while end < length:
+        # Saving the current codon in the reading frame
         codon = inputseq[start] + inputseq[start+1] + inputseq[end]
 
+        # Try incrementing number of times seen of current codon in frequency dictionary
         try:
             codonmap[codon] = codonmap[codon] + 1
+        # If an exception is thrown, current codon was not seen yet, add it to dictionary
         except:
             codonmap[codon] = 1
 
+        # Shift reading frame up three nucleotides
         start += 3
         end += 3
+
+        # Update number of codons read
         codonsread += 1
 
+    # Return number of codons read
     return codonsread
 
 
-def CreateDF(readname, readlength, codonmap):
+# Function used to convert codon frequency dictionary to a dataframe
+# Input: Name of read, length of read, codon frequency dictionary
+def create_df(readname, readlength, codonmap):
+    # Creating an empty list to hold all entries in the dataframe
     data = []
-    for codon in codonmap:
-        codondata = []
-        codondata.append(readname)
-        codondata.append(codon)
-        codondata.append(codonmap[codon])
-        localfrequency = 0;
 
+    # Iterating through all codons saved in codon map (aka frequency dictionary)
+    for codon in codonmap:
+        localfrequency = 0  # Variable for storing frequency of current codon's appearance in the read it came from
+
+        # Case for handling a 0 length read: if sequence length is not zero, calculate frequency
         if readlength != 0:
             localfrequency = round(float(codonmap[codon])/readlength, 5)
 
-        codondata.append(localfrequency)
-        codondata.append("N" in codon)
+        # Creating a list to store current entry in dataframe
+        # Cols: Name of read codon came from, codon sequence, times seen, frequency in read, contains an ambiguous read
+        codondata = [readname, codon, codonmap[codon], localfrequency, "N" in codon]
+
+        # Adding entry to dataframe list
         data.append(codondata.copy())
 
+    # Convert dataframe list into a dataframe
     df = pd.DataFrame(data, columns=['read_name', 'codon', 'times_seen', 'locol_codon_frequency', 'contains_ambiguous'])
 
+    # Print and return dataframe
     print(df)
     return df
 
 
+# Main function
+def main():
+    # Dictionary used to keep track of seen codons (frequency dictionary)
+    codonmap = {}
+    codoncounter = 0
+
+    # Hardcoded filename, will replace in future updates
+    filename = "RIBO-K90D1_R1_trimmed.fastq"
+
+    # Iterates through all entries in a fastq file and tracks number of codons seen
+    with open(filename) as in_handle:
+        # For every sequence in fastq file
+        for title, seq, qual in FastqGeneralIterator(in_handle):
+            # Analyze all codons in current sequence, update number of codons seen
+            codoncounter += track_codons(seq, codonmap, 0)
+
+        # Creating a name for this read by removing the .fastq portion of the input file
+        readname = filename[0:len(filename) - 6:]
+        # SQL has a problem with file names containing '-'. Ensuring these get replaced with '_' instead
+        readname = readname.replace("-", "_")
+
+        # Creating a dataframe of current sequence
+        df = create_df(readname, codoncounter, codonmap)
+        # Saving dataframe as a csv using the read name + ".csv"
+        df.to_csv(readname + ".csv")
 
 
-# Dictionary used to keep track of seen codons
-codonmap = {}
-codoncounter = 0
-filename = "RIBO-K90D1_R1_trimmed.fastq"
-
-# Iterates through all entries in a fastq file and tracks number of codons seen
-with open(filename) as in_handle:
-     for title, seq, qual in FastqGeneralIterator(in_handle):
-         codoncounter += TrackCodons(seq, codonmap)
-
-     readname = filename[0:len(filename)-6:]
-     readname = readname.replace("-", "_")
-     df = CreateDF(readname, codoncounter, codonmap)
-     df.to_csv(readname + ".csv")
+# This is where the main code starts
+if __name__ == "__main__":
+    main()
 
 
 # Might be a rule where certain number of nucleotides must be skipped before read
-# TODO: Update codon frequency
+# TODO: Update global codon frequency?
 # TODO: Remove ambiguous reads?
-
-
-
-
-
-
-
+# TODO: Replace hardcoded filename with an automated file read algorithm
